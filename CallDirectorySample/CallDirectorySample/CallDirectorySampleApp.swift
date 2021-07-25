266dae5bcf41f16a37aeb20b0febd01b07100b2c
kotetuco <paprapapra@gmail.com>
2021-07-25 15:20:18 +0900

    Add xcode project.

---
+++ CallDirectorySample/CallDirectorySample/CallDirectorySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallDirectorySampleApp.swift
+//  CallDirectorySample
+//
+//  Created by kotetu on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CallDirectorySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallDirectorySample/CallDirectorySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CallDirectorySample
+//
+//  Created by kotetu on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
